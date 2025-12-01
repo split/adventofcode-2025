e6a92f674ff7c492e221fbb774132fe721c311c2
Henri Lahtinen <splitti@gmail.com>
2025-12-01 16:04:46 +0200

    day01: solution for part 2

--- day01.hs
+++ day01.hs
@@ -1,11 +1,16 @@
 #!/usr/bin/env runghc
 module Main where
 
-main = interact (unlines . sequence [part1] . map ro . lines)
+main = interact (unlines . sequence [part1, part2] . map ro . lines)
 
-part1 = ("Part 1: " ++) . show . length . filter (== 0) . scanl step 50
+part1 = ("Part 1: " ++) . show . clicks
+part2 = ("Part 2: " ++) . show . clicks . method_0x434C49434B
 
-step acc x = rem (100 + acc + x) 100
+method_0x434C49434B xs = [signum r | r <- xs, x <- [1 .. abs r]]
+
+clicks = length . filter (== 0) . scanl step 50
+ where
+  step acc x = rem (100 + acc + x) 100
 
 ro :: String -> Int
 ro ('L' : xs) = -read xs

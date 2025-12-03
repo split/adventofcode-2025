ac110f63918a11723aace367168e74fe342e5d70
Henri Lahtinen <splitti@gmail.com>
2025-12-03 21:27:49 +0200

    day03: solution for the second part

--- day02.hs
+++ day02.hs
@@ -1,23 +1,25 @@
 #!/usr/bin/env runghc
-
 module Main where
 
+import Data.List (inits)
 import Data.List.Split (splitOn)
 
 main = interact (unlines . sequence [part1, part2] . concat . concatMap parse . lines)
 
+part1, part2 :: [Int] -> String
 part1 = ("Part 1: " ++) . show . sum . filter (elem 2 . repeats . show)
 part2 = ("Part 2: " ++) . show . sum . filter (not . null . repeats . show)
 
 repeats :: (Eq a) => [a] -> [Int]
-repeats xs = go 1
- where
-  n = length xs
-  go ptr
-    | ptr == n = []
-    | n `mod` ptr /= 0 = go (ptr + 1)
-    | concat (replicate (n `div` ptr) (take ptr xs)) == xs = n `div` ptr : go (ptr + 1)
-    | otherwise = go (ptr + 1)
+repeats xs = foldr go [] (inits xs)
+  where
+    go part acc
+      | ptr < 1 || ptr == len || len `mod` ptr /= 0 = acc
+      | and (zipWith (==) xs (cycle part)) = len `div` ptr : acc
+      | otherwise = acc
+      where
+        len = length xs
+        ptr = length part
 
 parse = map (range . splitOn "-") . splitOn ","
 
--- day03.hs
+++ day03.hs
@@ -1,13 +1,26 @@
 #!/usr/bin/env runghc
 module Main where
 
+import Control.Arrow ((&&&))
 import Data.Char (digitToInt)
+import Data.List (maximumBy, tails)
+import Data.Ord (comparing)
 
-main = interact (unlines . sequence [part1] . banks)
+main = interact (unlines . sequence [part1, part2] . banks)
 
-part1 = ("Part 1: " ++) . show . sum . map (maximum . joltages)
+part1, part2 :: [[Int]] -> String
+part1 = ("Part 1: " ++) . show . sum . map (joltage 2)
+part2 = ("Part 2: " ++) . show . sum . map (joltage 12)
 
-joltages [] = []
-joltages (x : xs) = ((10 * x +) <$> xs) ++ joltages xs
+joltage :: Int -> [Int] -> Int
+joltage n = foldl (\x y -> x * 10 + y) 0 . joltage' n
+
+joltage' :: Int -> [Int] -> [Int]
+joltage' n banks
+  | n <= 0 = []
+  | otherwise = j : joltage' (n - 1) banks'
+  where
+    i = max 1 (length banks - n + 1)
+    (j : banks') = maximumBy (comparing (take 1 &&& length)) (take i (tails banks))
 
 banks = map (map digitToInt) . lines

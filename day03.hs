eb441aa018beebdeb86d4ab9e6964119b30e733f
Henri Lahtinen <splitti@gmail.com>
2025-12-04 09:10:48 +0200

    day03: combine in same joltage

--- day03.hs
+++ day03.hs
@@ -12,15 +12,14 @@ part1, part2 :: [[Int]] -> String
 part1 = ("Part 1: " ++) . show . sum . map (joltage 2)
 part2 = ("Part 2: " ++) . show . sum . map (joltage 12)
 
-joltage :: Int -> [Int] -> Int
-joltage n = foldl (\x y -> x * 10 + y) 0 . joltage' n
+joltage = joltage' 0
 
-joltage' :: Int -> [Int] -> [Int]
-joltage' n banks
-  | n <= 0 = []
-  | otherwise = j : joltage' (n - 1) banks'
+joltage' :: Int -> Int -> [Int] -> Int
+joltage' acc n bank
+  | n <= 0 = acc
+  | otherwise = joltage' (acc * 10 + j) (n - 1) bank'
   where
-    i = max 1 (length banks - n + 1)
-    (j : banks') = maximumBy (comparing (take 1 &&& length)) (take i (tails banks))
+    i = max 1 (length bank - n + 1)
+    (j : bank') = maximumBy (comparing (take 1 &&& length)) (take i (tails bank))
 
 banks = map (map digitToInt) . lines

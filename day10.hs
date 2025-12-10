6be1093d4953472399f9078610d89cbceba05dc1
Henri Lahtinen <splitti@gmail.com>
2025-12-11 01:53:35 +0200

    day10: turn lights into a single number

--- day10.hs
+++ day10.hs
@@ -2,15 +2,15 @@
 module Main where
 
 import Data.Bifunctor (bimap)
-import Data.Bits (Bits (shiftR, xor, (.&.)))
-import Data.List (uncons, zipWith)
+import Data.Bits (Bits (bit, shiftL, shiftR, xor, (.&.), (.|.)))
+import Data.List (foldl', uncons, zipWith)
 import Data.Maybe (mapMaybe)
 import Data.Sequence (Seq (..), (><))
 import Data.Sequence qualified as Seq
 import Data.Set qualified as S
 
 data Machine = Machine
-  {presses :: Int, lights :: [Int], buttons :: Seq [Int]}
+  {presses :: Int, lights :: Int, buttons :: Seq [Int]}
   deriving (Show, Eq, Ord)
 
 main = interact (unlines . sequence [part1] . parse)
@@ -22,28 +22,23 @@ runMachine :: Machine -> Machine
 runMachine im@Machine {buttons} = press ((im,) <$> buttons) S.empty
   where
     press ((m, b) :<| next) seen
-      | all matches (lights m) = m
-      | (b, lights m) `S.member` seen = press next seen
+      | (lights m, b) `S.member` seen = press next seen
+      | matches (lights m) = m
       | otherwise =
           press
             (next >< ((pressButton m b,) <$> buttons))
-            ((b, lights m) `S.insert` seen)
+            ((lights m, b) `S.insert` seen)
 
-pressButton m button = m {presses = presses m + 1, lights = zipWith (toggle button) [0 ..] (lights m)}
+pressButton m b = m {presses = presses m + 1, lights = toggle (lights m) b}
 
-toggle :: [Int] -> Int -> Int -> Int
-toggle button i state
-  | i `elem` button = state `xor` 1
-  | otherwise = state
+toggle lights button = lights `xor` foldl' (.|.) 0 (bit . (* 2) <$> button)
 
-matches v = ((v `xor` (v `shiftR` 1)) .&. 1) == 0
+matches v = v `shiftR` 1 == 0 || ((v .&. 1) == 1 && matches (v `shiftR` 2))
 
 parse = map (uncurry (Machine 0)) . mapMaybe go . lines . concatMap fmt
   where
-    go = (map light `bimap` (Seq.fromList . init . map read) <$>) . uncons . words
-    light v = case v of
-      '#' -> 2
-      '.' -> 0
+    go = (lights `bimap` (Seq.fromList . init . map read) <$>) . uncons . words
+    lights = foldr (\v acc -> acc `shiftL` 2 .|. case v of '#' -> 2; '.' -> 3) 0
     fmt c = case c of
       c | c `elem` "[]" -> ""
       c | c `elem` "{(" -> "["

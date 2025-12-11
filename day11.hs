b37e7829b699892a82113e54da6c318620f30ae3
Henri Lahtinen <splitti@gmail.com>
2025-12-11 16:09:06 +0200

    day11: solution for second part

--- day11.hs
+++ day11.hs
@@ -1,23 +1,33 @@
 #!/usr/bin/env runghc
 module Main where
 
-import Data.List
+import Control.Monad.State (evalState, gets, modify)
+import Data.List (uncons)
 import Data.Map.Strict qualified as M
-import Data.Maybe
-import Data.Set qualified as S
+import Data.Maybe (mapMaybe)
 
 type Grid = M.Map String [String]
 
-main = interact (unlines . sequence [part1] . parse)
+main = interact (unlines . sequence [part1, part2] . parse)
 
-part1 = ("Part 1: " ++) . show . outs
+part1, part2 :: Grid -> String
+part1 = ("Part 1: " ++) . show . ("you" `outs` "out")
+part2 = ("Part 2: " ++) . show . product . applyAll ["svr" `outs` "fft", "fft" `outs` "dac", "dac" `outs` "out"]
 
-outs :: Grid -> Int
-outs g = go S.empty "you"
+applyAll fs g = map ($ g) fs
+
+outs :: String -> String -> Grid -> Int
+outs from to g = evalState (go from) M.empty
   where
-    go seen to
-      | to == "out" = 1
-      | otherwise = sum (go (S.insert to seen) <$> g M.! to)
+    go = memoState go'
+    go' cur
+      | cur == to = pure 1
+      | cur `M.member` g = sum <$> mapM go (g M.! cur)
+      | otherwise = pure 0
 
 parse :: String -> Grid
 parse = M.mapKeys init . M.fromList . mapMaybe (uncons . words) . lines
+
+memoState f k = maybe compute pure =<< gets (M.lookup k)
+  where
+    compute = f k >>= \v -> modify (M.insert k v) >> pure v

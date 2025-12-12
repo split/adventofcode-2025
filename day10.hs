a171a27d60cf7ebe54da41798feae9cc0673f1a9
Henri Lahtinen <splitti@gmail.com>
2025-12-12 13:59:06 +0200

    day10: increments to lights and parse joltages

--- day10.hs
+++ day10.hs
@@ -1,4 +1,6 @@
 #!/usr/bin/env runghc
+{-# LANGUAGE LambdaCase #-}
+
 module Main where
 
 import Data.Bifunctor (bimap)
@@ -10,36 +12,38 @@ import Data.Sequence qualified as Seq
 import Data.Set qualified as S
 
 data Machine = Machine
-  {presses :: Int, lights :: Int, buttons :: Seq [Int]}
+  {presses :: Int, lights :: [Int], buttons :: Seq [Int], jotages :: [Int]}
   deriving (Show, Eq, Ord)
 
-main = interact (unlines . sequence [part1] . parse)
+main = interact (unlines . sequence [part1, part2] . parse)
 
 part1 :: [Machine] -> String
 part1 = ("Part 1: " ++) . show . sum . map (presses . runMachine)
+
+part2 = ("Part 2: " ++) . show
 
 runMachine :: Machine -> Machine
 runMachine im@Machine {buttons} = press ((im,) <$> buttons) S.empty
   where
     press ((m, b) :<| next) seen
       | (lights m, b) `S.member` seen = press next seen
-      | matches (lights m) = m
+      | allON (lights m) = m
       | otherwise =
           press
             (next >< ((pressButton m b,) <$> buttons))
             ((lights m, b) `S.insert` seen)
 
-pressButton m b = m {presses = presses m + 1, lights = toggle (lights m) b}
+pressButton m b = m {presses = presses m + 1, lights = toggle b (lights m)}
 
-toggle lights button = lights `xor` foldl' (.|.) 0 (bit . (* 2) <$> button)
+toggle b = zipWith (\i l -> if i `elem` b then (l + 2) `xor` 1 else l) [0 ..]
 
-matches v = v `shiftR` 1 == 0 || ((v .&. 1) == 1 && matches (v `shiftR` 2))
+allON = all (\v -> (v .&. 1) == 1)
 
-parse = map (uncurry (Machine 0)) . mapMaybe go . lines . concatMap fmt
+parse = map (\(l, v) -> Machine 0 l (Seq.fromList $ init v) (last v)) . mapMaybe go . lines . concatMap fmt
   where
-    go = (lights `bimap` (Seq.fromList . init . map read) <$>) . uncons . words
-    lights = foldr (\v acc -> acc `shiftL` 2 .|. case v of '#' -> 2; '.' -> 3) 0
-    fmt c = case c of
+    go = (lights `bimap` map read <$>) . uncons . words
+    lights = map (\case '#' -> 0; '.' -> 1)
+    fmt = \case
       c | c `elem` "[]" -> ""
       c | c `elem` "{(" -> "["
       c | c `elem` "})" -> "]"
